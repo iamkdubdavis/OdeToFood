b360ad2778f19d022966bf4b46d94d3b2570458a
Kimberly Davis <kwatsondavis@gmail.com>
2015-10-04 17:18:47 -0400

    Added more unit tests

--- OdeToFood.Tests/Features/RestaurantRater.cs
+++ OdeToFood.Tests/Features/RestaurantRater.cs
@@ -16,7 +16,31 @@ namespace OdeToFood.Tests.Features
         public RatingResult ComputeRating(int numberOfReviews)
         {
             var result = new RatingResult();
-            result.Rating = 4;
+            result.Rating = (int)_restaurant.Reviews.Average(r => r.Rating);
+            return result;
+        }
+
+        public RatingResult ComputeWeightedRate(int numberOfReviews)
+        {
+            var reviews = _restaurant.Reviews.ToArray();
+            var result = new RatingResult();
+            var counter = 0;
+            var total = 0;
+
+            for (int i = 0; i < reviews.Count(); i++) {
+                if (i < reviews.Count() / 2)
+                {
+                    counter += 2;
+                    total += reviews[i].Rating * 2;
+                }
+                else
+                {
+                    counter += 1;
+                    total += reviews[i].Rating;
+                }
+            }
+
+            result.Rating = total / counter;
             return result;
         }
     }
--- OdeToFood.Tests/Features/UnitTest1.cs
+++ OdeToFood.Tests/Features/UnitTest1.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OdeToFood.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 /*
 A restaurant's overall rating can be calculated using various
@@ -24,16 +25,61 @@ namespace OdeToFood.Tests.Features
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Computes_Result_For_One_Review()
         {
-            var data = new Restaurant();
-            data.Reviews = new List<RestaurantReview>();
-            data.Reviews.Add(new RestaurantReview() { Rating = 4 });
+            // Arrange
+            var data = BuildRestaurantAndReviews(ratings: 4);
+
+            // Act
+            var rater = new RestaurantRater(data);
+            var result = rater.ComputeRating(10);
+
+            // Assert
+            Assert.AreEqual(4, result.Rating);
+        }
+
+        [TestMethod]
+        public void Computes_Result_For_Two_Reviews()
+        {
+            var data = BuildRestaurantAndReviews(ratings: new[] { 4, 8 });
 
             var rater = new RestaurantRater(data);
             var result = rater.ComputeRating(10);
 
-            Assert.AreEqual(4, result.Rating);
+            Assert.AreEqual(6, result.Rating);
+        }
+
+        //[TestMethod]
+        //public void Rating_Includes_Only_First_N_Reviews()
+        //{
+        //    var data = BuildRestaurantAndReviews(1, 1, 1, 10, 10, 10);
+
+        //    var rater = new RestaurantRater(data);
+        //    var result = rater.ComputeRating(10);
+
+        //    Assert.AreEqual(5, result.Rating);
+        //}
+
+        [TestMethod]
+        public void Weighted_Averaging_For_Two_Reviews()
+        {
+            var data = BuildRestaurantAndReviews(3, 9);
+
+            var rater = new RestaurantRater(data);
+            var result = rater.ComputeRating(10);
+
+            Assert.AreEqual(6, result.Rating);
+        }
+
+        private Restaurant BuildRestaurantAndReviews(params int[] ratings)
+        {
+            var restaurant = new Restaurant();
+
+            restaurant.Reviews =
+                ratings.Select(r => new RestaurantReview { Rating = r })
+                    .ToList();
+
+            return restaurant;
         }
     }
 }
